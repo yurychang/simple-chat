a6ca0cdfa7e097a8c559fca8e95751d7b9027b06
Yury <yuury.chang@gmail.com>
2024-01-30 00:20:48 +0800

    u

--- apps/chat-server/src/features/chat/socket-handler.ts
+++ apps/chat-server/src/features/chat/socket-handler.ts
@@ -3,6 +3,7 @@ import { MessageData } from './types';
 
 export const messageHandler = (socket: Socket, io: Server) => {
   socket.on('message', (message: MessageData) => {
+    console.log('message', message);
     socket.to(message.roomId).emit('message', message);
   });
 };
--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -1,21 +1,11 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect, useState } from 'react';
+import { ElementRef, useEffect, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import { Outlet } from 'react-router-dom';
 import { z } from 'zod';
 
 import { Button } from '@/components/ui/button';
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
 import { socket } from '@/libs/socket';
 
 const formSchema = z.object({
@@ -23,13 +13,6 @@ const formSchema = z.object({
 });
 
 export function Home() {
-  // const [username, setUsername] = useState('');
-  // const updateName = () => {
-  //   username && socket.emit('updateName', username);
-  // };
-
-  // const [message, setMessage] = useState('');
-
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -41,41 +24,40 @@ export function Home() {
     form.register('message');
   });
 
+  const messageInput = useRef<ElementRef<'div'>>(null);
+
+  const onInput = (e: React.FormEvent<HTMLDivElement>) => {
+    form.setValue('message', e.currentTarget.textContent || '');
+  };
+
   function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
     socket.emit('message', values.message);
+    form.reset();
+    if (messageInput.current) {
+      messageInput.current.textContent = '';
+    }
   }
   return (
     <>
       <div>
         <Outlet />
         <div className="max-w-screen-lg mx-auto">
-          {/* <Input
-            type="text"
-            placeholder="username"
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <div className="flex justify-end mt-3">
-            <Button onClick={updateName}>Update name</Button>
-          </div> */}
           <div className="flex flex-col justify-end min-h-screen">
             <div className="my-5">
               <Form {...form}>
-                <form
-                  className="flex space-y-8"
-                  onSubmit={form.handleSubmit(onSubmit)}
-                >
+                <form className="flex" onSubmit={form.handleSubmit(onSubmit)}>
                   <FormField
                     control={form.control}
                     name="message"
-                    render={({ field }) => (
+                    render={() => (
                       <>
-                        <FormItem>
+                        <FormItem className="flex-1">
                           <FormControl>
                             <div
+                              ref={messageInput}
                               contentEditable
                               className="flex-1 px-3 py-2 border rounded-md min-h-4"
-                              {...field}
+                              onInput={onInput}
                             />
                           </FormControl>
                         </FormItem>
