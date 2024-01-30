51ce94a97fe365800f4c84e431bda4e703d42ea9
PeterSYChang <145960221+PeterSYChang@users.noreply.github.com>
2024-01-30 10:17:01 +0800

    Refactor home page layout and add sidebar

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -41,40 +41,44 @@ export function Home() {
     <>
       <div>
         <Outlet />
-        <div className="max-w-screen-lg mx-auto">
-          <div className="flex flex-col justify-end min-h-screen">
-            <div className="my-5">
-              <Form {...form}>
-                <form className="flex" onSubmit={form.handleSubmit(onSubmit)}>
-                  <FormField
-                    control={form.control}
-                    name="message"
-                    render={() => (
-                      <>
-                        <FormItem className="flex-1">
-                          <FormControl>
-                            <div
-                              ref={messageInput}
-                              contentEditable
-                              className="flex-1 px-3 py-2 border rounded-md min-h-4"
-                              onInput={onInput}
-                            />
-                          </FormControl>
-                        </FormItem>
-                      </>
-                    )}
-                  />
-                  <Button className="ml-3" type="submit">
-                    Submit
-                  </Button>
-                </form>
-              </Form>
-            </div>
+        <div className="grid min-h-screen grid-cols-3 gap-3 px-5">
+          <Sidebar></Sidebar>
+          <div className="relative h-full col-span-2 py-5">
+            <Form {...form}>
+              <form
+                className="absolute left-0 right-0 flex items-end w-full gap-3 bottom-5"
+                onSubmit={form.handleSubmit(onSubmit)}
+              >
+                <FormField
+                  control={form.control}
+                  name="message"
+                  render={() => (
+                    <>
+                      <FormItem className="flex-1">
+                        <FormControl>
+                          <div
+                            ref={messageInput}
+                            contentEditable
+                            className="px-3 py-[7px] border rounded-md min-h-[38px]"
+                            onInput={onInput}
+                          />
+                        </FormControl>
+                      </FormItem>
+                    </>
+                  )}
+                />
+                <Button type="submit">Submit</Button>
+              </form>
+            </Form>
           </div>
         </div>
       </div>
     </>
   );
+}
+
+function Sidebar() {
+  return <div className="py-5">Sidebar</div>;
 }
 
 export default Home;
